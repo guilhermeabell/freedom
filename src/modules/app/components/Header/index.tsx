4860e2b7c5ef8745c07ea7b66e49d4a7eb16f021
guilhermeabell <gpeuvis@gmail.com>
2024-02-10 23:07:08 -0300

    feat(develop): created Header component

---
+++ src/modules/app/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import { Heart } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+
+export function Header() {
+  const navigate = useNavigate()
+
+  const handleNavigateToWishlist = () => {
+    navigate('/wishlist')
+  }
+
+  const handleNavigateToHome = () => {
+    navigate('/')
+  }
+
+  return (
+    <header className="bg-brand-purple w-full h-16 z-50 shadow-md">
+      <div className="container mx-auto px-4 py-2 flex justify-between items-center">
+        <div className="flex items-center py-3">
+          <a onClick={handleNavigateToHome} className="cursor-pointer">
+            <img src="https://static.netshoes.com.br/2.89.4/netshoesbr/images/logo.png" alt="Netshoes" className="w-auto" />
+          </a>
+        </div>
+        <div className="flex items-center space-x-4">
+          <button onClick={handleNavigateToWishlist} className="text-brand-white font-semibold flex justify-between gap-2">
+            <Heart />
+            Lista de desejos
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
