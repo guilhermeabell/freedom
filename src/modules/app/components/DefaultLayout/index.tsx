e06773e589b1660572625d83ae74810f20df69f1
guilhermeabell <gpeuvis@gmail.com>
2024-02-11 13:01:32 -0300

    refactor(develop): adjust DefaultLayout

--- src/modules/app/components/DefaultLayout/index.tsx
+++ src/modules/app/components/DefaultLayout/index.tsx
@@ -6,13 +6,13 @@ export function DefaultLayout() {
   return (
     <div className="mx-auto">
       <Header />
-      <div className="mt-4">
+      <div className="mt-10">
         <div className="container mx-auto px-4 py-2 border-b-2 border-brand-lightGray">
           <BreadcrumbMenu />
         </div>
       </div>
-      <div className="container mx-auto px-4 py-2">
-        <div className="flex justify-center items-center h-96">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-center items-center">
           <Outlet />
         </div>
       </div>
