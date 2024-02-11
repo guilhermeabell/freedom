80d7f5ddb4dbbf54f78efa1617ca676b9c0412ab
guilhermeabell <gpeuvis@gmail.com>
2024-02-11 17:22:23 -0300

    feat(develop): integrating productCard component with the api

--- src/modules/productsList/page.tsx
+++ src/modules/productsList/page.tsx
@@ -1,20 +1,25 @@
+import { useEffect } from 'react'
+import { useAppStore } from '../app/api/app.store'
 import { ProductCard } from '../app/components/ProductCard'
+import { fetchProducts } from '../common/utils/fetcher'
 
 export default function ProductsList() {
+  const { products, listProducts } = useAppStore()
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      const formattedProducts = await fetchProducts()
+      listProducts(formattedProducts)
+    }
+
+    loadProducts()
+  }, [listProducts])
+
   return (
     <div className="flex flex-wrap justify-center gap-4 p-4">
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      {products.map((product, index) => (
+        <ProductCard key={index} product={product} />
+      ))}
     </div>
   )
 }
