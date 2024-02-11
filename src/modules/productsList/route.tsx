f4b2a36a4106ee033dde388f303f7ff266f4a032
guilhermeabell <gpeuvis@gmail.com>
2024-02-10 23:10:01 -0300

    feat(develop): created productsList initial structure

---
+++ src/modules/productsList/route.tsx
@@ -0,0 +1,18 @@
+import { Suspense, lazy } from 'react'
+import { RouteObject } from 'react-router-dom'
+
+// eslint-disable-next-line react-refresh/only-export-components
+const PageComponent = lazy(() => import('./page'))
+
+const route: RouteObject = {
+  index: true,
+  Component: () => (
+    <Suspense
+      fallback={<h1>Carregando...</h1>}
+    >
+      <PageComponent />
+    </Suspense>
+  ),
+}
+
+export default route
