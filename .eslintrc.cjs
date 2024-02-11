564ae5b0641492513d6d2500e1bf2eb65662d637
guilhermeabell <gpeuvis@gmail.com>
2024-02-11 17:13:53 -0300

    chore(develop): adjust linting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,16 +14,6 @@ module.exports = {
         sourceType: 'module',
       },
       plugins: ['react-refresh'],
-      rules: {
-        'react-refresh/only-export-components': 'warn',
-        'import/no-extraneous-dependencies': [
-          'error',
-          {
-            devDependencies: ['**/*.d.ts', '**/*.test.tsx', './src/tests/**/*.{ts,tsx}', 'vite.config.ts'],
-          },
-        ],
-        'react/jsx-props-no-spreading': 'off',
-      },
       files: ['./**/*.{ts,tsx}'],
       settings: {
         'import/resolver': {
@@ -38,6 +28,7 @@ module.exports = {
       files: ['./**/*.test.{ts,tsx}'],
       rules: {
         '@typescript-eslint/no-explicit-any': 'off',
+        'react-refresh/only-export-components': 'off',
       },
     },
   ],
