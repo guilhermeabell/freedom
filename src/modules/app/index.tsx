5fa2ca0a8a92d07ccae742192845ee5a4b60ef47
guilhermeabell <gpeuvis@gmail.com>
2024-02-11 14:03:43 -0300

    chore(develop): adding api env and logger

--- src/modules/app/index.tsx
+++ src/modules/app/index.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { routes } from './routes'
 
-const api = import.meta.env.VITE_API_BASE_URL
+const apiUrl = import.meta.env.VITE_API_BASE_URL
 
-console.log(api)
+console.log('API URL:', apiUrl)
 
 export function App() {
   return <RouterProvider router={createBrowserRouter(routes)} />
