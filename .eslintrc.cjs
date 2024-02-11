68a9d7bb8178c4d2e3469a421aea2947a74af879
guilhermeabell <gpeuvis@gmail.com>
2024-02-11 17:08:08 -0300

    chore(develop): adjust linting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,44 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-  },
+  overrides: [
+    {
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+      ],
+      parserOptions: {
+        ecmaVersion: 'latest',
+        project: ['./tsconfig.json', './tsconfig.node.json'],
+        sourceType: 'module',
+      },
+      plugins: ['react-refresh'],
+      rules: {
+        'react-refresh/only-export-components': 'warn',
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            devDependencies: ['**/*.d.ts', '**/*.test.tsx', './src/tests/**/*.{ts,tsx}', 'vite.config.ts'],
+          },
+        ],
+        'react/jsx-props-no-spreading': 'off',
+      },
+      files: ['./**/*.{ts,tsx}'],
+      settings: {
+        'import/resolver': {
+          alias: {
+            map: [['@', './src']],
+            extensions: ['.ts', '.tsx'],
+          },
+        },
+      },
+    },
+    {
+      files: ['./**/*.test.{ts,tsx}'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
+  ],
 }
